d3d109754129e495fe85b9df0895d39efbc81433
Pedro Galan <pedro.galan@piksel.com>
2020-06-12 12:29:40 +0100

    Add dashboard

--- src/main/java/es/pedrogalan/time/hystrix/TimeHystrixDashboardApplication.java
+++ src/main/java/es/pedrogalan/time/hystrix/TimeHystrixDashboardApplication.java
@@ -2,8 +2,10 @@ package es.pedrogalan.time.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 public class TimeHystrixDashboardApplication {
 
 	public static void main(String[] args) {
