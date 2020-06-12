421b1920ad5a90daf4923075ad45de06eaa30026
Pedro Galan <pedro.galan@piksel.com>
2020-06-12 11:53:56 +0100

    Spring boot app skeleton

---
+++ src/main/java/es/pedrogalan/time/hystrix/TimeHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package es.pedrogalan.time.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeHystrixDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/pedrogalan/time/hystrix/TimeHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pedrogalan.time.hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
